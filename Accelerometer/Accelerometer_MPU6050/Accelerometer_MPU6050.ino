b8c00c80399b206a9f273258ec3a2bbd68e49098
Alexei Smith <alexei.smith@sony.com>
2024-09-23 14:06:28 +0100

    Fix EOLs

--- Accelerometer/Accelerometer_MPU6050/Accelerometer_MPU6050.ino
+++ Accelerometer/Accelerometer_MPU6050/Accelerometer_MPU6050.ino
@@ -24,12 +24,12 @@ void gyro_signals(void) {
   Wire.write(0x05);
   Wire.endTransmission(); 
   Wire.beginTransmission(0x68);
-  Wire.write(0x1B); 
-  Wire.write(0x8); 
-  Wire.endTransmission(); 
+  Wire.write(0x1B);
+  Wire.write(0x08);
+  Wire.endTransmission();
   Wire.beginTransmission(0x68);
   Wire.write(0x43);
-  Wire.endTransmission();
+  Wire.endTransmission(); 
   Wire.requestFrom(0x68,6);
   int16_t GyroX=Wire.read()<<8 | Wire.read();
   int16_t GyroY=Wire.read()<<8 | Wire.read();
