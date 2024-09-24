d43a9aeed0c0cb27212d097288f4b4d0f51350c3
Christopher Buchanan <christopher.george.buchanan@gmail.com>
2024-09-24 09:05:20 +0100

    Add files via upload

---
+++ PitchDetection/PitchDetection.ino
@@ -0,0 +1,99 @@
+
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+#include "Team1_glissandoDetect.h"
+
+const int myInput = AUDIO_INPUT_MIC;
+
+// GUItool: begin automatically generated code
+AudioInputI2S audioInput;           // xy=295,80
+AudioAnalyzeNoteFrequency notefreq; // xy=528,86
+AudioSynthWaveformSine sinewave;
+AudioControlSGTL5000 audioShield;
+
+float f0Frame[FRAME_SIZE];
+PitchSlideResult result;
+float note;
+float prob;
+
+// GUItool: end automatically generated code
+
+// Connect either the live input or synthesized sine wave
+AudioConnection patchCord1(audioInput, 0, notefreq, 0);
+// AudioConnection patchCord1(sinewave, 0, notefreq, 0);
+
+void push(float arr[], int size, float newValue)
+{
+
+  for (int i = 1; i < size; i++)
+  {
+    arr[i - 1] = arr[i]; // Shift each element left
+  }
+
+  arr[size - 1] = newValue; // Place the first element at the end
+}
+
+void setup()
+{
+  // Audio connections require memory to work.  For more
+  // detailed information, see the MemoryAndCpuUsage example
+  AudioMemory(30);
+
+  // Enable the audio shield and set the output volume.
+  audioShield.enable();
+  audioShield.inputSelect(myInput);
+  audioShield.volume(0.5);
+  audioShield.micGain(20);
+
+  // Begin with threshold for pitch detection
+  notefreq.begin(.5);
+
+  // Create a synthetic sine wave, for testing
+  // To use this, edit the connections above
+  sinewave.amplitude(0.8);
+  sinewave.frequency(263);
+
+  // fill frame with zeros
+  for (auto i = 0; i < FRAME_SIZE; i++)
+  {
+    f0Frame[i] = 0.0f;
+  }
+
+}
+
+void loop()
+{
+
+  if (notefreq.available())
+  {
+    note = notefreq.read();
+    prob = notefreq.probability();
+  }
+
+  // Serial.printf("Note: %3.2f | Probability: %.2f\n", note, prob);
+
+  // plot note
+  // Serial.print("Note:");
+  // Serial.print(note);
+  // Serial.print("\n");
+
+  // add F0 to vector
+  push(f0Frame, FRAME_SIZE, note);
+
+  // Serial.printf("frame: %g %g %g %g %g\n", f0Frame[0], f0Frame[1], f0Frame[2], f0Frame[3], f0Frame[4]);
+
+  // decide if FRAME contains a glissando
+  result = detect_pitch_slide(f0Frame);
+
+  Serial.print("length:");
+  Serial.print(result.length);
+  Serial.print("\n");
+
+// have loop run roughly every 1ms
+delay(1);
+
+}
---
+++ PitchDetection/Team1_glissandoDetect.cpp
@@ -0,0 +1,72 @@
+#include "math.h"
+#include "Team1_glissandoDetect.h"
+#include <SerialFlash.h>
+
+static void computeRateOfChange(float arr[FRAME_SIZE], float rates[FRAME_SIZE - 1])
+{
+    for (int i = 1; i < FRAME_SIZE; i++)
+    {
+        rates[i - 1] = arr[i] - arr[i - 1];
+    }
+}
+
+static void findLargestConstantChunk(float arr[FRAME_SIZE], float threshold, PitchSlideResult *result)
+{
+    int startIdx = 0;
+    int length = 0;
+    int currentStart = 0;
+    int currentLength = 0;
+
+    for (int i = 1; i < FRAME_SIZE; i++)
+    {
+
+        // if rateOfChange value is "constant" and "non-zero"
+        if ((fabs(arr[i] - arr[currentStart]) <= threshold) && ((arr[i] > 0.5) || (arr[i] < -0.5)))
+        {
+            // Serial.printf("test\n");
+            currentLength++;
+        }
+        else
+        {
+
+            // check if current segment is longer than longest segment we have so far
+            if (currentLength > length)
+            {
+                startIdx = currentStart;
+                length = currentLength;
+            }
+
+            currentStart = i;
+            currentLength = 0;
+        }
+    }
+
+    // Final check at the end of the loop
+    if (currentLength > length)
+    {
+        startIdx = currentStart;
+        length = currentLength;
+    }
+
+    result->length = length;
+}
+
+PitchSlideResult detect_pitch_slide(float frequencies[FRAME_SIZE])
+{
+
+    PitchSlideResult result = {NO_SLIDE, 0, 0.0f}; // Initialize result
+    float min_freq = 0;
+    float max_freq = 0;
+    float rates[FRAME_SIZE];
+    int *glissStart;
+    int *glissLength;
+
+    computeRateOfChange(frequencies, rates);
+
+    // Serial.printf("frequencies: %g %g %g %g %g\n", frequencies[0], frequencies[1], frequencies[2], frequencies[3], frequencies[4]);
+    // Serial.printf("rateOfChange: %g %g %g %g %g\n", rates[0], rates[1], rates[2], rates[3], rates[4]);
+
+    findLargestConstantChunk(rates, 50.0f, &result);
+
+    return result;
+}
